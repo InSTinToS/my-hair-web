31b13711f4c625463a7b7818da50acc63f9dc30f
Miguel <miguelandradebarreto2@gmail.com>
2022-08-26 11:19:20 -0300

    :bug: fix type error

--- src/store/users/index.ts
+++ src/store/users/index.ts
@@ -1,16 +1,12 @@
 import { update } from './reducers/update'
 
-import type { TExtraReducers } from 'types/redux.types'
-
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = { loading: undefined, users: undefined }
 
-const extraReducers: TExtraReducers<any> = builder => {}
-
 const usersStore = createSlice({
   initialState,
-  extraReducers,
+
   name: 'users-store',
   reducers: { update }
 })
